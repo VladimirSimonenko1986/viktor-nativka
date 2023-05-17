57786468ce08e729c63b90154374eb19048724c6
Vladimir <vova.simonenko@mail.ru>
2023-05-17 12:38:04 +0500

    lesson#2 methods arrays

--- index.js
+++ index.js
@@ -55,6 +55,58 @@ console.log(students.map((st)=> ({name: st.name, scores: st.scores})))
 console.log(selfMadeMap(students, (st)=> ({name: st.name, scores: st.scores})))
 
 
+const selfMadeFilter  = (array, conditionFn)=> {
+     const result = []
+    for (let i=0; i < array.length; i++) {
+       if(conditionFn(array[i]) === true) {
+           result.push(array[i])
+
+       }
+    }
+    return result
+
+}
+
+console.log(students.filter(st=> st.scores >= 100))
+
+console.log(selfMadeFilter(students,  st=> st.scores >= 100))
+
+const selfMadePush = (array, ...els) => {
+    console.log(els)
+    for(let i = 0; i < els.length; i++) {
+        array[array.length] = els[i]
+    }
+    return array.length
+}
+
+const arr = [1,2,3,4,5]
+console.log(selfMadePush(arr, 6,7,8))
+
+
+console.log(arr.push(6,7,8))
+console.log(arr)
+
+const selfMadeIncludes = (array, value) => {
+    for (let i = 0; i < array.length ; i++) {
+        if (array[i] === value) {
+            return true;
+        }
+    }
+    return false;
+};
+
+console.log(selfMadeIncludes(arr, 4))
+
+console.log(arr.includes(4))
+
+
+
+
+
+
+
+
+
 
 
 
